9fa28fb7bc4227ecefc6f251980576cb7e6fcaa7
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-03-20 16:49:13 -0500

    Challenge FirstApp

---
+++ 01-vite/src/Components/FirstApp.jsx
@@ -0,0 +1,12 @@
+import './FirstApp.css'
+
+function FirstApp() {
+    return (
+        <>
+            <h1> Mi titulo </h1>
+            <span>10</span>
+        </>
+    )
+}
+
+export default FirstApp;
