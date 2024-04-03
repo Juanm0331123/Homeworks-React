a14c4fce00a7575a8e7b13c14ecab11213992b5b
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-04-03 16:49:16 -0500

    Segundo challenge en proceso

--- 01-vite/src/Components/ComponentApp.jsx
+++ 01-vite/src/Components/ComponentApp.jsx
@@ -1,8 +1,21 @@
 import './ComponentApp.css'
+import { useState } from 'react';
 
-const categories = ['First category', 'Second category', 'Third category'];
+const initialsCategories = ['First category', 'Second category', 'Third category'];
 
 export const ComponentApp = () => {
+    const [categories, setCategories] = useState(initialsCategories);
+    const [category, setCategory] = useState('');
+
+    const handleInputChange = (event) => {
+        setCategory(event.target.value);
+    }
+
+    const handleAddCategory = () => {
+        setCategories([...categories, category]);
+        setCategory('');
+    }
+
     return (
         <>
             <h1>GifExpert</h1>
@@ -17,6 +30,9 @@ export const ComponentApp = () => {
                     )
                 }
             </ol>
+
+            <input type="text" value={category} onChange={handleInputChange} />
+            <button onClick={handleAddCategory}>Add category</button>
         </>
     )
 }
